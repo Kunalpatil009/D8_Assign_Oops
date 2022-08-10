080008b3a2cf87c831a247a81f429273b992222e
Kunalpatil009 <009kunalpatil@gmail.com>
2022-08-10 19:49:48 +0530

    till_max_condition_added

--- Emp_Wages_Oops/src/EmpWagesOops/EmpWagesUC1.java
+++ Emp_Wages_Oops/src/EmpWagesOops/EmpWagesUC1.java
@@ -4,36 +4,41 @@ public class EmpWagesUC1 {
     static int WAGESPERHOURS = 20;
     static int FULLDAYHOURS = 8;
     static int PARTTIMEHOURS = 4;
-    static int TotalWages = 0;
+    static int TOTALWAGES = 0;
+
+    static int WORKINGHOURS ;
     static void emp_Check() {
         double emp = Math.floor(Math.random() * 10) % 3;
             switch ((int) emp){
                 case 1:
                     System.out.println("Employee is Present For Full Time");
-                TotalWages = WAGESPERHOURS * FULLDAYHOURS;
-                System.out.println( "TotalWages :" + TotalWages);
+                    TOTALWAGES = WAGESPERHOURS * FULLDAYHOURS;
+                System.out.println( "TotalWages :" + TOTALWAGES);
                 break;
                 case 2:
                     System.out.println("Employee is Present For Part Time");
-                TotalWages = WAGESPERHOURS * PARTTIMEHOURS;
-                System.out.println( "TotalWages :" + TotalWages);
+                    TOTALWAGES = WAGESPERHOURS * PARTTIMEHOURS;
+                System.out.println( "TotalWages :" + TOTALWAGES);
                 break;
                 default:
                     System.out.println("Employee is Absent");
                 }
             }
             static int WAGES;
-             static int monthlyWages(int workingDaysPerMonth) {
+            static  int TOTALWORKHOURS;
+             static void monthlyWages(int MAXWORKINGDAYS ,int MAXWORKINGHOURS ) {
             int day = 1;
-                while ( day <= workingDaysPerMonth) {
+                while (  day <= MAXWORKINGDAYS && WORKINGHOURS <= MAXWORKINGHOURS) {
                     System.out.println("day :" + day);
                     emp_Check();
-                    WAGES += TotalWages;
+                    WAGES += TOTALWAGES;
+                    TOTALWORKHOURS += WORKINGHOURS  ;
                     day++;
 
                 }
                  System.out.println("Total Monthly Wages :" +WAGES);
-                return workingDaysPerMonth;
+                 System.out.println("Total Monthly Hours :" +TOTALWORKHOURS);
+
             }
 //            if (emp == 1) {
 //                System.out.println("Employee is Present");
@@ -45,7 +50,7 @@ public class EmpWagesUC1 {
     public static void main(String[] Kunal) {
         System.out.println("Welcome To Employee Wages Computation Programme");
 //            emp_Check();
-            monthlyWages(20) ;
+            monthlyWages(20, 100) ;
 
     }
 }
