33d0770326b756f22bdc2f780ec31fd47c973d79
Kunalpatil009 <009kunalpatil@gmail.com>
2022-08-10 18:18:56 +0530

    [UC3]_parttime_wages_added_[UC4]_using_switch_case

--- Emp_Wages_Oops/src/EmpWagesOops/EmpWagesUC1.java
+++ Emp_Wages_Oops/src/EmpWagesOops/EmpWagesUC1.java
@@ -1,21 +1,35 @@
 package EmpWagesOops;
 
 public class EmpWagesUC1 {
-        static void emp_Check(int  ISPRESENT) {
+        static void emp_Check() {
             System.out.println("Welcome To Employee Wages Computation Programme");
             int WAGESPERHOURS = 20;
             int FULLDAYHOURS = 8;
-            double emp = Math.floor(Math.random() * 10) % 2;
-            if (emp == 1) {
-                System.out.println("Employee is Present");
+            int PARTTIMEHOURS = 4;
+            double emp = Math.floor(Math.random() * 10) % 3;
+            switch ((int) emp){
+                case 1:
+                    System.out.println("Employee is Present For Full Time");
                int TotalWages = WAGESPERHOURS * FULLDAYHOURS;
                 System.out.println( "TotalWages :" + TotalWages);
-            } else {
-                System.out.println("Employee is Absent");
+                break;
+                case 2:
+                    System.out.println("Employee is Present For Part Time");
+                TotalWages = WAGESPERHOURS * PARTTIMEHOURS;
+                System.out.println( "TotalWages :" + TotalWages);
+                break;
+                default:
+                    System.out.println("Employee is Absent");
+                }
             }
-        }
-
+//            if (emp == 1) {
+//                System.out.println("Employee is Present");
+//               int TotalWages = WAGESPERHOURS * FULLDAYHOURS;
+//                System.out.println( "TotalWages :" + TotalWages);
+//            } else {
+//                System.out.println("Employee is Absent");
+//            }
     public static void main(String[] Kunal) {
-            emp_Check(1);
+            emp_Check();
     }
 }
