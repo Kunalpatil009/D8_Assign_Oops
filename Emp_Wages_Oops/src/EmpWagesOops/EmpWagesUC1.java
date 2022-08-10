636d4eaec0241eaf6fbcc64fbf28a9df7cccb757
Kunalpatil009 <009kunalpatil@gmail.com>
2022-08-10 17:42:27 +0530

    emp_daily_wages_added

--- Emp_Wages_Oops/src/EmpWagesOops/EmpWagesUC1.java
+++ Emp_Wages_Oops/src/EmpWagesOops/EmpWagesUC1.java
@@ -1,19 +1,21 @@
 package EmpWagesOops;
 
-
-
 public class EmpWagesUC1 {
-    static void emp_Check(){
-        System.out.println("Welcome To Employee Wages Computation Programme");
-    double emp = Math.floor(Math.random()*10) % 2;
-    if(emp == 1){
-        System.out.println("Employee is Present");
-    }
-    else {
-        System.out.println("Employee is Absent");
-    }
-    }
+        static void emp_Check(int  ISPRESENT) {
+            System.out.println("Welcome To Employee Wages Computation Programme");
+            int WAGESPERHOURS = 20;
+            int FULLDAYHOURS = 8;
+            double emp = Math.floor(Math.random() * 10) % 2;
+            if (emp == 1) {
+                System.out.println("Employee is Present");
+               int TotalWages = WAGESPERHOURS * FULLDAYHOURS;
+                System.out.println( "TotalWages :" + TotalWages);
+            } else {
+                System.out.println("Employee is Absent");
+            }
+        }
+
     public static void main(String[] Kunal) {
-     emp_Check();
+            emp_Check(1);
     }
 }
